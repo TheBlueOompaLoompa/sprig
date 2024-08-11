e600279b0c2b202a44b4716f74f521d9cb063870
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2024-08-11 02:05:03 -0500

    Remove unused file

--- firmware/hose/src/bootstrap.py
+++
@@ -1,9 +0,0 @@
-from sprig import Sprig
-
-def main(manual = True):
-	sprig = Sprig()
-
-	if sprig.settings['autostart'] or manual:
-		sprig.launch('com.hackclub.sprig.Launcher')
-		while True:
-			if sprig.loop(): return
